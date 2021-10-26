01ea21fc03abeefd18ec0a131f3d1050069eb036
tiago-yoneda <xyonedax@gmail.com>
2021-10-26 09:36:51 -0300

    colocadas as dependencias basicas usando sprint initializer

---
+++ src/main/java/com/codenation/central/CentralApplication.java
@@ -0,0 +1,13 @@
+package com.codenation.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codenation/central/CentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenation.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

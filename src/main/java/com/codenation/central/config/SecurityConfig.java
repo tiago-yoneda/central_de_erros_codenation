907933c78d297a52d7fcff55d3a7ad7b62c5fbf4
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-11-01 14:07:24 -0300

    Token de acesso para os 4 usuarios

--- src/main/java/com/codenation/central/config/SecurityConfig.java
+++ src/main/java/com/codenation/central/config/SecurityConfig.java
@@ -1,34 +1,40 @@
-//package com.codenation.central.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//
-//@EnableWebSecurity
-//@EnableAuthorizationServer
-//@EnableResourceServer
-//class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Bean
-//    @Override
-//    protected AuthenticationManager authenticationManager() throws Exception {
-//        return super.authenticationManager();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//}
+package com.codenation.central.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("adrianoforcellinipedretti").password("123456").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("layokaminski").password("123456").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("johnealves").password("123456").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("xyonedax").password("123456").roles("ADMIN");
+
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
